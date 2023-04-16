f8a55480afb2748cd5e060a0c1f3bff73cc3a467
Byzus <87544061+Byzus@users.noreply.github.com>
2023-04-16 19:11:03 +0200

    Check if player is online to avoid errors

--- src/main/java/dev/byzus/byzuscells/command/UnJailPlayerCommand.java
+++ src/main/java/dev/byzus/byzuscells/command/UnJailPlayerCommand.java
@@ -7,14 +7,13 @@ import dev.rollczi.litecommands.command.execute.Execute;
 import dev.rollczi.litecommands.command.permission.Permission;
 import dev.rollczi.litecommands.command.route.Route;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 @Route(name = "unjail")
 @Permission("byzuscells.unjail")
 public class UnJailPlayerCommand {
 
     @Execute(required = 1)
-    void execute(CommandSender sender, @Arg @Name("target") Player target) {
+    void execute(CommandSender sender, @Arg @Name("target") String target) {
         PlayerJailManager.unJail(sender, target);
     }
 
--- src/main/java/dev/byzus/byzuscells/manager/PlayerJailManager.java
+++ src/main/java/dev/byzus/byzuscells/manager/PlayerJailManager.java
@@ -26,51 +26,56 @@ public class PlayerJailManager {
 
     public static void jail(CommandSender sender, String target, int borderSize) {
         Player player = Bukkit.getPlayer(target);
-        if (player != null) {
-            Location loc = player.getLocation();
-            sender.sendMessage(LanguageManager.JAILED_PLAYER);
+        if (player == null) {
+            sender.sendMessage(LanguageManager.CANNOT_FIND_PLAYER.append(Component.text(target)));
+            return;
+        }
+        Location loc = player.getLocation();
+        sender.sendMessage(LanguageManager.JAILED_PLAYER);
 
-            if (sender instanceof Player) {
-                player.sendMessage(LanguageManager.YOU_HAVE_BEEN_JAILED_BY.append(Component.text(target)));
-            } else if (!(sender instanceof Player)) {
-                player.sendMessage(LanguageManager.YOU_HAVE_BEEN_JAILED);
-            }
+        if (sender instanceof Player) {
+            player.sendMessage(LanguageManager.YOU_HAVE_BEEN_JAILED_BY.append(Component.text(target)));
+        } else if (!(sender instanceof Player)) {
+            player.sendMessage(LanguageManager.YOU_HAVE_BEEN_JAILED);
+        }
 
-            player.setGameMode(GameMode.ADVENTURE);
-            player.playSound(player, JAIL_SOUND, 0.25F, 1.0F);
+        player.setGameMode(GameMode.ADVENTURE);
+        player.playSound(player, JAIL_SOUND, 0.25F, 1.0F);
 
-            for (int x = -borderSize; x <= borderSize; x++) {
-                for (int y = -borderSize; y <= borderSize; y++) {
-                    for (int z = -borderSize; z <= borderSize; z++) {
-                        Location blockLoc = loc.toBlockLocation().clone().add(x, y, z);
-                        jails.put(Triple.of(blockLoc, blockLoc.getBlock().getBlockData(), blockLoc.getBlock().getState()), player.getUniqueId());
-                        blockLoc.getBlock().setType(MATERIAL);
-                        if (borderSize == 1) {
-                            jails.put(Triple.of(player.getEyeLocation().toBlockLocation().add(0, 1, 0), player.getEyeLocation().toBlockLocation().getBlock().getBlockData(), player.getEyeLocation().toBlockLocation().getBlock().getState()), player.getUniqueId());
-                            player.getEyeLocation().toBlockLocation().add(0, 1, 0).getBlock().setType(MATERIAL);
-                        }
-                        player.getLocation().toBlockLocation().getBlock().setType(Material.AIR);
-                        player.getEyeLocation().toBlockLocation().getBlock().setType(Material.AIR);
+        for (int x = -borderSize; x <= borderSize; x++) {
+            for (int y = -borderSize; y <= borderSize; y++) {
+                for (int z = -borderSize; z <= borderSize; z++) {
+                    Location blockLoc = loc.toBlockLocation().clone().add(x, y, z);
+                    jails.put(Triple.of(blockLoc, blockLoc.getBlock().getBlockData(), blockLoc.getBlock().getState()), player.getUniqueId());
+                    blockLoc.getBlock().setType(MATERIAL);
+                    if (borderSize == 1) {
+                        jails.put(Triple.of(player.getEyeLocation().toBlockLocation().add(0, 1, 0), player.getEyeLocation().toBlockLocation().getBlock().getBlockData(), player.getEyeLocation().toBlockLocation().getBlock().getState()), player.getUniqueId());
+                        player.getEyeLocation().toBlockLocation().add(0, 1, 0).getBlock().setType(MATERIAL);
                     }
+                    player.getLocation().toBlockLocation().getBlock().setType(Material.AIR);
+                    player.getEyeLocation().toBlockLocation().getBlock().setType(Material.AIR);
                 }
             }
-        } else {
-            sender.sendMessage(LanguageManager.CANNOT_FIND_PLAYER.append(Component.text(target)));
         }
     }
 
-    public static void unJail(CommandSender sender, Player target) {
+    public static void unJail(CommandSender sender, String target) {
+        Player player = Bukkit.getPlayer(target);
+        if (player == null) {
+            sender.sendMessage(LanguageManager.CANNOT_FIND_PLAYER.append(Component.text(target)));
+            return;
+        }
         sender.sendMessage(LanguageManager.PLAYER_HAS_BEEN_UNJAILED);
-        target.sendMessage(LanguageManager.YOU_HAVE_BEEN_UNJAILED);
-        target.playSound(target, UNJAIL_SOUND, 0.25F, 0.5F);
+        player.sendMessage(LanguageManager.YOU_HAVE_BEEN_UNJAILED);
+        player.playSound(player, UNJAIL_SOUND, 0.25F, 0.5F);
 
-        if (target.getPreviousGameMode() == null) {
-            target.setGameMode(GameMode.SURVIVAL);
-        } else if (!(target.getPreviousGameMode() == null)) {
-            target.setGameMode(target.getPreviousGameMode());
+        if (player.getPreviousGameMode() == null) {
+            player.setGameMode(GameMode.SURVIVAL);
+        } else if (!(player.getPreviousGameMode() == null)) {
+            player.setGameMode(player.getPreviousGameMode());
         }
         for (Map.Entry<Triple<Location, BlockData, BlockState>, UUID> entry : jails.entrySet()) {
-            if (entry.getValue().equals(target.getUniqueId())) {
+            if (entry.getValue().equals(player.getUniqueId())) {
                 entry.getKey().getFirst().getBlock().setBlockData(entry.getKey().getSecond());
                 entry.getKey().getFirst().getBlock().getState().update(true);
             }
